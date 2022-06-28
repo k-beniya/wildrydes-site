231b44112503355410ea09276306a40cb8ccddda
K-Beniya <k-beniya@telema.jp>
2022-06-28 11:53:57 +0900

    changed config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_YWKt08dvh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ubpvl27i1jhrtsj5rn4o634j2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
