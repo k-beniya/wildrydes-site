f8c95406c2466be95b3c300330fc38614248c2ca
K-Beniya <k-beniya@telema.jp>
2022-06-28 14:44:23 +0900

    changes invokeUrl

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@ window._config = {
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://9wjppq3y43.execute-api.ap-northeast-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+https://9wjppq3y43.execute-api.ap-northeast-1.amazonaws.com/prod
